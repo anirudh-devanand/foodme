8f0ee9f82fff9c2a494f199cf310128d924c8615
Sidak <sidak@server.ca>
2025-01-18 15:01:07 -0800

    Changed dir structure

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+from flask import Flask, request, jsonify
+import google.generativeai as genai
+from datetime import datetime, timedelta
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
